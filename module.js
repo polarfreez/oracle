223848db9008f75ae06e4e4457206e4dbda49678
StoltZ <83966759+stoltz-dev@users.noreply.github.com>
2024-03-04 15:50:35 -0300

    Update module.js

--- module.js
+++ module.js
@@ -498,13 +498,20 @@ function fadeInOut(DOMElement, fadeType, displayType) {
 }
 
 async function createImage(prompt) {
-	const app = await client("AP123/Playground-v2.5");
-	const result = await app.predict("/generate_image", [		
-					prompt, // string  in 'Enter your image prompt' Textbox component		
-					50, // number (numeric value between 1 and 75) in 'Number of Inference Steps' Slider component		
-					7, // number (numeric value between 1 and 10) in 'Guidance Scale' Slider component
-	]);
-
+	const app = await client("multimodalart/stable-cascade");
+	const result = await app.predict("/run", [		
+					prompt, // string  in 'Prompt' Textbox component		
+					"Hello!!", // string  in 'Negative prompt' Textbox component		
+					Math.random() * 2147483646 + 1, // number (numeric value between 0 and 2147483647) in 'Seed' Slider component		
+					1024, // number (numeric value between 1024 and 1536) in 'Width' Slider component		
+					1024, // number (numeric value between 1024 and 1536) in 'Height' Slider component		
+					20, // number (numeric value between 10 and 30) in 'Prior Inference Steps' Slider component		
+					4, // number (numeric value between 0 and 20) in 'Prior Guidance Scale' Slider component		
+					4, // number (numeric value between 4 and 12) in 'Decoder Inference Steps' Slider component		
+					0, // number (numeric value between 0 and 0) in 'Decoder Guidance Scale' Slider component		
+					1, // number (numeric value between 1 and 2) in 'Number of Images' Slider component
+		]);
+	
 	console.log(result.data);
 
 	return result.data;
